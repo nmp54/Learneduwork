28af6fc6b77a8319979e93e6705762ecec08ed50
nmp54 <mulyaputrinur@gmail.com>
2024-11-20 17:38:46 +0700

    [test]Add script for valid login functionality on saucedemo.com

--- cypress/e2e/0-personal-learn/automationlogin-invalid.test.cy.js
+++ cypress/e2e/0-personal-learn/automationlogin-invalid.test.cy.js
@@ -1,26 +1,29 @@
 /// <reference types="cypress" />
-///melakukan login dengan username dan password yang salah
+
+// Test case for logging in with an incorrect username and password
 describe('Login with Wrong Password', () => {
+    // Visit the Saucedemo website
     it('Visit the website', () => {
         cy.visit('https://www.saucedemo.com/')
         cy.url().should('include', 'saucedemo')
-
     });
-    ///menggunakan id untuk memasukkan username password yang salah
-    it('Should fill username wrong', () => {
+
+    // Input an incorrect username using the username ID
+    it('Should fill incorrect username', () => {
         cy.get('#user-name').clear()
         cy.get('#user-name').type('username')
     });
-    ///menggunakan id untuk memasukkan password yang salah
-    it('Should fill password wrong', () => {
+
+    // Input an incorrect password using the password ID
+    it('Should fill incorrect password', () => {
         cy.get('#password').clear()
         cy.get('#password').type('password')
-
     });
-    //menggunakan fixture untuk memasukkan username dan password 
-    it('Should try to login', () => {
+
+    // Use fixture data to input an incorrect username and password
+    it('Should try to login with incorrect credentials', () => {
         cy.fixture('user').then((user) => {
-            ///memakai method untuk dapat mengimplementasikan username dan password nya
+            // Use variables from the fixture file for incorrect credentials
             const username = user.username_wrong
             const password = user.password_wrong
 
@@ -30,8 +33,10 @@ describe('Login with Wrong Password', () => {
             cy.get('#password').clear()
             cy.get('#password').type(password)
 
+            // Click the login button
             cy.contains('Login').click()
-            //menggunakan class untuk memasukkan detail errror
+
+            // Verify the error message using its class
             cy.get('.error-message-container').contains('Epic sadface: Username and password do not match any user in this service')
         })
     })
